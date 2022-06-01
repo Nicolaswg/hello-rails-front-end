2e5d1c4d403d2ee752815a07f86ddaf33cd91ea6
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-06-01 12:21:51 -0400

    Set up component

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMessageFromAPI } from '../redux/Greetings/greeting';
 
 function Greetings() {
+  const selected = useSelector((state) => state.messages);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(getMessageFromAPI);
+  };
+
   return (
-    <div>greetings</div>
+    <div>
+      greetings:
+      {selected}
+      <button type="button" onClick={handleClick}>Generate messages</button>
+    </div>
   );
 }
 
